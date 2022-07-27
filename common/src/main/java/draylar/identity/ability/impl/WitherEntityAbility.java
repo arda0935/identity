2f3ffa95f680a16da1753f56360218e7fbe1537e
arda0935 <100447376+arda0935@users.noreply.github.com>
2022-07-27 12:33:02 +0300

    Update WitherEntityAbility.java

--- common/src/main/java/draylar/identity/ability/impl/WitherEntityAbility.java
+++ common/src/main/java/draylar/identity/ability/impl/WitherEntityAbility.java
@@ -31,8 +31,3 @@ public class WitherEntityAbility extends IdentityAbility<WitherEntity> {
         return Items.WITHER_SKELETON_SKULL;
     }
 }
-@Override
-    public int getCooldown(WitherEntity entity) {
-        return 1 * 1;
-    }
-}
